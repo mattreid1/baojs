11915c6acf193eafcc402bed3799428176205880
Matt Reid <6438781+mattreid1@users.noreply.github.com>
2022-07-21 13:16:32 +0100

    Added .prettierrc

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -2,18 +2,18 @@ import { suite, Test } from "uvu";
 import * as assert from "uvu/assert";
 
 export const expect = (value: any) => {
-    return {
-        toBeInstanceOf: (type: any) => {
-          assert.is(value instanceof type, true);
-        },
-        toBe: (expected: any) => {
-          assert.is(value, expected);
-        },
-        toEqual: (expected: any) => {
-          assert.equal(value, expected);
-        }
-    };
-}
+  return {
+    toBeInstanceOf: (type: any) => {
+      assert.is(value instanceof type, true);
+    },
+    toBe: (expected: any) => {
+      assert.is(value, expected);
+    },
+    toEqual: (expected: any) => {
+      assert.equal(value, expected);
+    },
+  };
+};
 
 export const describe = (name: string, cb: (it: Test) => void) => {
   const test = suite(name);
